7b5b011145071954c89babcabf87ea891b517141
Merrick Christensen <merrick.christensen@gmail.com>
2014-05-01 20:40:21 -0600

    cleanup(Reporters): removed shameful typos

--- src/reporters/console.js
+++ src/reporters/console.js
@@ -1,7 +1,7 @@
 (function (root, factory) {
     if (typeof define === 'function' && define.amd) {
         define(factory);
-    } else if (typeof exports === 'object') {bv 
+    } else if (typeof exports === 'object') {
         module.exports = factory();
     } else {
         root.ConsoleReporter = factory();
